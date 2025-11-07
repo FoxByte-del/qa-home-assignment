5d967c684977efe90f9753e20bff930f45d515b4
Kertu <kertu@MacBookPro.lan>
2025-11-07 20:51:00 +0200

    Unit tests setup

---
+++ CardValidation.Web.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace CardValidation.Web.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    // MethodName_Scenario_ExpectedBehavior()
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
