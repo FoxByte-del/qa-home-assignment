01005ceaa0e37436b041efc2d6ae9b7b09b439b1
Kertu <kertu@MacBookPro.lan>
2025-11-07 23:58:37 +0200

    Fix setup

---
+++ CardValidation.Core.Tests/Services/CardValidationServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using CardValidation.Core.Services;
+using NUnit.Framework;
+
+namespace CardValidation.Core.Tests.Services;
+
+[TestFixture]
+public class CardValidationServiceTests
+{
+    private CardValidationService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new CardValidationService();
+    }
+
+    [TestCase("Meow Purr")]
+    public void ValidateOwner_ValidOwner_ReturnsTrue(string owner)
+    {
+        Assert.That(_service.ValidateOwner(owner), Is.True);
+    }
+
+    [TestCase("")]
+    public void ValidateOwner_OwnerEmpty_ReturnsFalse(string owner)
+    {
+        Assert.That(_service.ValidateOwner(owner), Is.False);
+    }
+}
